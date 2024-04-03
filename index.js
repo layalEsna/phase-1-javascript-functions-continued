7da77d047e48262522976fa48fcf7c215df07e50
Amene <layla_1380@yahoo.com>
2024-04-02 22:32:22 -0500

    closure, anonymous functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(string = 'roller-skate') {
+    return `This Saturday, I want to ${string}!`
+
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+
+
+}
+
+function wrapAdjective(highlight = '*') {
+    return function (quality = 'special') {
+        return `You are ${highlight}${quality}${highlight}!`
+    }
+}
+wrapAdjective('||')('hard')
